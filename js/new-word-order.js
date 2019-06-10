f09adf057f105ec81fa89a012e075646709a7217
Ben Bennett-Cauchon and Everett Lee-Wuollet <everett@>
2019-06-10 14:10:25 -0700

    add sorting functionality and empty last word counter on new search. Tested on bible and lord of the rings

--- js/new-word-order.js
+++ js/new-word-order.js
@@ -1,11 +1,12 @@
 $(function(){
   $("form#groceries").submit(function(event) {
     event.preventDefault();
-    // var text = $("#inputText").val()
-    var text = "dddd. ccc bb, a dddd dddd dddd d ccc ccc bb , ."
-    var words = text.split(/[ ,.]/)
-    var punctuationChars = [" ", ",", "."]
-    words.filter(word => !punctuationChars.includes(word))
+    $(".wordTable").empty();
+    var text = $("#inputText").val()
+    var words = text.split(/\W/)
+    // var punctuationChars = [" ", ",", ".", ""]
+    // words = words.filter(word => !punctuationChars.includes(word))
+    words = words.filter(word => word !== "")
     var wordcount = new Map()
     words.forEach(function(word){
       var count = wordcount.get(word);
@@ -21,6 +22,9 @@ $(function(){
     words.sort((word1, word2) => wordcount.get(word1) - wordcount.get(word2))
     console.log(wordcount);
     console.log(words)
+    words.forEach(function(words){
+      $(".wordTable").append("<tr><td>" + words +"</td><td>" + wordcount.get(words) + "</td></tr>")
+    })
     // debugger
   });
 });
